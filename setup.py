58908d5e991352a323d38d72ac219c0c6d83546e
JMiahMan <jmiahman@unity-linux.org>
2018-11-28 19:55:07 -0700

    Modify setup script

--- setup.py
+++ setup.py
@@ -8,10 +8,15 @@ setup(name='vmaudit',
       version='0.1',
       description='Audit your vms with ease',
       long_description=readme(),
+      long_description_content_type='text/markdown',
       url='https://github.com/jmiahman/webapi-fun',
       author='JMiahMan',
       author_email='JMiahMan@unity-linux.org',
       license='MIT',
       packages=['vmaudit'],
       include_package_data=True,
-      zip_safe=False)
+      zip_safe=False,
+      scripts=['vmaudit/vm-audit'],
+      data_files=[('/etc', ['vmaudit/vm-audit.cfg']),
+                  ('/usr/bin', ['vmaudit/vm-audit'])]
+      )
